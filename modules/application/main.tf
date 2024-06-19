2ca4a68c8eac9e52b40c9645fcaf1d58a66d3529
Noam Strauss <noamstrauss@gmail.com>
2024-06-19 16:12:46 +0300

    RFE
    Adding support for bringing your own service principal
    
    - Added variables `create_service_principal`, `service_principal_object_id`, `application_id`, `application_password` in variables.tf & modules/application
    - Added `Using Existing Service Principal` section in README.md

--- modules/application/main.tf
+++ modules/application/main.tf
@@ -1,13 +1,16 @@
 resource "azuread_application" "aqua_cspm_scanner" {
+  count        = var.create_service_principal ? 1 : 0
   display_name = var.application_name
 }
 
 resource "azuread_application_password" "aqua_cspm_scanner" {
+  count             = var.create_service_principal ? 1 : 0
   display_name      = "rbac"
-  application_id    = "/applications/${azuread_application.aqua_cspm_scanner.object_id}"
+  application_id    = "/applications/${azuread_application.aqua_cspm_scanner[0].object_id}"
   end_date_relative = "262980h"
 }
 
 resource "azuread_service_principal" "aqua_cspm_scanner" {
-  client_id = azuread_application.aqua_cspm_scanner.client_id
+  count     = var.create_service_principal ? 1 : 0
+  client_id = azuread_application.aqua_cspm_scanner[0].client_id
 }
